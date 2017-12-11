e02042491320d1bb8fab334598525f41d102facd
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-11 15:58:51 +0900

    Java Hello 프로그램 작성

---
+++ java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	// class 다음 Hello는 파일이름과 같아야 함 
+	// main: method(메서드). 프로그램의 시작점 
+	// String[] args: argument, parameter
+	public static void main(String[] args) {
+		
+		// system.out.printIn 메서드: 괄호 안의 글자를 출력
+		System.out.println("Hello, World");
+	}
+}

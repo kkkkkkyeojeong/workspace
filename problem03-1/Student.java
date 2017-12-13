a559868bedbc384dbedb0e1a1d472dacf86740ef
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-13 11:44:57 +0900

    Student 두번째

---
+++ problem03-1/Student.java
@@ -0,0 +1,24 @@
+class Student{
+	int studentID;
+	String name;
+	int age;
+	String major;
+	int grade;
+
+	public Student(int studentID, String name, int age, String major, int grade){
+		this.studentID = studentID;
+		this.name = name;
+		this.age = age;
+		this.major = major;
+		this.grade = grade;
+	}
+	
+	void Study(){
+		System.out.println(this.name + " (이)가 " + this.major + " 공부를 한다.");
+	}
+	
+	void Introduce(){
+		System.out.println("저는 " + this.major + this.grade + "학년 재학중인 " 
+							+ this.name + " 입니다.");
+	}
+}	
---
+++ problem03-1/TestDrive.java
@@ -0,0 +1,17 @@
+public class TestDrive {
+	public static void main(String[] args){
+		
+		Student A = new Student(2017111, "홍길동", 20, "영문학과", 1);
+		Student B = new Student(2015111, "김영희", 22, "철학과", 3);
+		Student C = new Student(2016111, "박철수", 21, "디자인과", 2);
+	
+	A.Study();
+	A.Introduce();
+	
+	B.Study();
+	B.Introduce();
+	
+	C.Study();
+	C.Introduce();
+	}
+}

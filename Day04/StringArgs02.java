9118c5294cd24997a318e068dcc6c2f9d95ff98b
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-14 10:37:59 +0900

    args배열

--- Day04/StringArgs02.java
+++ Day04/StringArgs02.java
@@ -2,7 +2,7 @@ public class StringArgs02{
 	public static void main(String[] args){
 		System.out.println(args[0] + " " + args[1] + " " + args[2]);	
 
-		// argsÀÇ ¹è¿­Å©±â¸¦ ¾Ë¼öÀÖÀ½
+		// argsì˜ ë°°ì—´í¬ê¸°ë¥¼ ì•Œìˆ˜ìˆìŒ
 		System.out.println(args.length);
 	}
 }

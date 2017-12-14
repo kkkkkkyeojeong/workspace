371a6f13a188b56f040b6b93f64901b2d50f4a55
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-14 10:35:20 +0900

    args배열

---
+++ Day04/StringArgs02.java
@@ -0,0 +1,10 @@
+public class StringArgs02{
+	public static void main(String[] args){
+		System.out.println(args[0] + " " + args[1] + " " + args[2]);	
+
+		// args의 배열크기를 알수있음
+		System.out.println(args.length);
+	}
+}
+
+	

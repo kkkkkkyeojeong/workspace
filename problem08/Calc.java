0d88b52086cb0cb6b24275da2cc91e4327418cf6
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-15 14:50:19 +0900

    계산 수정22

--- problem08/Calc.java
+++ problem08/Calc.java
@@ -1,19 +1,22 @@
 class Calc{
 
 	int add(int a, int b) {
-		return a + b;
+		int result1 = a + b ;
+		return result1;
 	}
 	
 	int sub(int a, int b) {
-		return a - b;
+		int result2 = a - b ;
+		return result2;
 	}
 
 	int mul(int a, int b) {
-		return a*b;
+		int result3 = a * b ;
+		return result3;
 	}
 
 	int div(int a, int b) {
-		return a/b;
+		int result4 = a / b ;
+		return result4;
 	}
-
 }

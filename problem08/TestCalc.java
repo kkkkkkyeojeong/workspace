e5c214186ab7b03d8f5d83c6feb21a162d4038b4
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-15 15:05:25 +0900

    계산 args적용

--- problem08/TestCalc.java
+++ problem08/TestCalc.java
@@ -2,18 +2,24 @@ public class TestCalc {
 
 	public static void main(String[] args) {
 
+
 	Calc A = new Calc();
 
-	int result1 = A.add(10, 5);
+	int ta = Integer.parseInt(args[0]);
+	int tb = Integer.parseInt(args[1]);
+	
+
+
+	int result1 = A.add(ta, tb);
 	System.out.println(result1);
 
-	int result2 = A.sub(10, 5);
+	int result2 = A.sub(ta, tb);
 	System.out.println(result2);
 
-	int result3 = A.mul(10, 5);
+	int result3 = A.mul(ta, tb);
 	System.out.println(result3);
 
-	int result4 = A.div(10, 5);
+	int result4 = A.div(ta, tb);
 	System.out.println(result4);
 
 	}

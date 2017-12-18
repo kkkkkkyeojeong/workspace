47e368095722fd696c3ca4c4d25d78dceddd5113
kkkkkkyeojeong <duwjd6539@gmail.com>
2017-12-18 16:47:20 +0900

    break 사용

---
+++ Day06/Problem03.java
@@ -0,0 +1,43 @@
+public class Problem03 {
+
+	public static void main(String[] args) {
+
+		// H 출력 
+		for (char i = 65; i < 91; i++) {
+				
+			if (i == 'H') {
+				System.out.print(i);
+				break;
+			}
+		}
+
+		// E 출력 
+		for (char i = 65; i < 91; i++) {
+				
+			if (i == 'E') {
+				System.out.print(i);
+				break;
+			}
+		}
+
+		// LL 출력 
+		for (char i = 65; i < 91; i++) {
+				
+			if (i == 'L') {
+				System.out.print(i);
+				System.out.print(i);
+				break;
+			}
+		}
+
+		// O 출력 
+		for (char i = 65; i < 91; i++) {
+				
+			if (i == 'O') {
+				System.out.print(i);
+				break;
+			}
+		}
+
+	}
+}
